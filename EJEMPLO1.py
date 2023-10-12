e0b113e04f0898f28ef87a9f5232a1f8114ba9f1
ELIERUTSC <145230238+ELIERUTSC@users.noreply.github.com>
2023-10-11 20:21:22 -0600

    Update EJEMPLO1.py
    HOLA

--- EJEMPLO1.py
+++ EJEMPLO1.py
@@ -8,14 +8,3 @@ class Student(User):
 self.id = id if id else uuid.uuid4()  
         self.score = score  
     def send_email(self):  
-      
-        # Aquí puedes implementar la lógica para enviar un correo electrónico.  
-print(f"Correo electrónico enviado a {self.email}")  
-
-# Crear una instancia de Student llamada Student1  
-Student1 = Student("sem", "sem.hernandez@utsc.edu.mx")  
-Student1.send_email()  
-
-# Crear otra instancia de Student llamada Student2 con un correo electrónico  
-Student2 = Student(email="otro.correo@utsc.edu.mx")  
-Student2.send_email()  
